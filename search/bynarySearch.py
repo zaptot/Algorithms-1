29d8b31085bfe67af3b38437750bcb3315fc8659
VeronikaBogdanovich <veronicka.angel2012@yandex.by>
2018-09-26 22:24:55 +0300

    change bynary search

--- search/bynarySearch.py
+++ search/bynarySearch.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[35]:
+# In[15]:
 
 
 import time
@@ -48,12 +48,34 @@ def interpolationSearch(arr, item):
             first = middle + 1
     return result
 
+def quickSort(arr, first, last):
+    if first < last:
+        q = findQ(arr, first, last)
+        quickSort(arr, first, q-1)
+        quickSort(arr, q+1, last)    
+    return arr;
 
-# In[36]:
+def findQ(arr, first, last):
+    pivot = arr[last]
+    index_of_smaller = first - 1
+    for j in range(first,last):
+        if (arr[j] <= pivot):
+            index_of_smaller += 1
+            temp = arr[index_of_smaller]
+            arr[index_of_smaller] = arr[j]
+            arr[j] = temp
+    temp = arr[index_of_smaller + 1]
+    arr[index_of_smaller + 1] = arr[last]
+    arr[last] = temp
+
+    return (index_of_smaller + 1);   
+
+
+# In[16]:
 
 
 a=[1,2,31,4,5,16,7,8,9,56,554,245,234,14,3,6,22,11,26,17,13]
-a.sort()
+quickSort(a,0,len(a)-1)
 
 # print(binarySearch(a, NUM_TO_FIND))
 # print(interpolationSearch(a, NUM_TO_FIND))
