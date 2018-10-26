89068fd499205495d61b5c4fd81e396e6ccfb244
VeronikaBogdanovich <veronicka.angel2012@yandex.by>
2018-10-26 18:33:03 +0300

    Update Euler/Euler .py

--- Euler/Euler .py
+++ Euler/Euler .py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[77]:
+# In[23]:
 
 
 def findCycle(graph):
-    if is_euler(graph):
+    if isEuler(graph):
         stack = []
         stack.append(0)
         res = []
@@ -23,7 +23,7 @@ def findCycle(graph):
         return None
 
 
-# In[78]:
+# In[24]:
 
 
 def isEuler(graph):
@@ -33,65 +33,72 @@ def isEuler(graph):
     return True
 
 
-# In[79]:
+# In[25]:
 
 
-# Задание графов(матрица инцидентности)
-withoutCycle0 = [
-    [0, 1, 0, 1, 0],
-    [1, 0, 1, 0, 0],
-    [0, 1, 0, 1, 0],
-    [1, 0, 1, 0, 1],
-    [0, 0, 0, 1, 0],
-]
-
-withCycle0 = [
-    [0, 1, 0, 1],
-    [1, 0, 1, 0],
-    [0, 1, 0, 1],
-    [1, 0, 1, 0],
-]
-
-withoutCycle = [
-    [0, 1, 0, 1],
-    [1, 0, 0, 0],
-    [0, 0, 0, 0],
-    [1, 0, 0, 0],
-]
-
-withCycle = [
-    [0, 1, 0, 0, 1],
-    [1, 0, 1, 0, 0],
-    [0, 1, 0, 1, 0],
-    [0, 0, 1, 0, 1],
-    [1, 0, 0, 1, 0],
-]
+def addEdge(graph):
+    print('Введите вершины ребра: ')
+    v1=(int(input())-1)
+    v2=(int(input())-1)
+    graph[v1][v2]=1;
+    graph[v2][v1]=1;
 
 
-# In[80]:
+# In[30]:
 
 
-print(isEuler(withCycle))
-print(isEuler(withoutCycle))
+def removeVertex(graph):
+    k= (int(input('Какую вершину удаляем? '))-1)
+    for i in range(N):
+        for j in range (k,N-1):
+            graph[i][j]=graph[i][j+1]
+        graph[i].pop(N-1)
+    graph.pop(k)
+    for i in graph:
+        print(*i)
+
+def removeEdge(graph):
+    print('Введите вершины ребра, которое будет удалено: ')
+    vertex1=(int(input())-1)
+    vertex2=(int(input())-1)
+    graph[vertex1][vertex2]=0
+    graph[vertex2][vertex1]=0
+    for i in graph:
+        print(*i)
 
 
-# In[81]:
+# In[37]:
 
 
-print(findCycle(withoutCycle0))
-print(findCycle(withCycle0))
-print(findCycle(withoutCycle))
-print(findCycle(withCycle))
+# Задание графа (матрица инцидентности)
+import random
+import numpy as np
+N=int(input('число строк и столбцов: '))
+        
+graph=[[0 for _ in range(N)] for _ in range(N)]
 
+print("Хотите добавить ребра? y/n ")
+agree=str(input())
+while(agree=="y"):
+    addEdge(graph)
+    agree=str(input("Ввести еще ребро? y/n "))
 
-# In[ ]:
+for i in graph:
+    print(*i)
 
+print("Хотите удалить ребра? y/n ")
+agree=str(input())
+while(agree=="y"):
+    removeEdge(graph)
+    agree=str(input("Удалить еще ребро? y/n "))
+    
+print("Хотите удалить вершины? y/n ")
+agree=str(input())
+while(agree=="y"):
+    removeVertex(graph)
+    N=N-1
+    agree=str(input("Удалить еще вершину? y/n "))
 
-
-
-
-# In[ ]:
-
-
-
+print(isEuler(graph))
+print(findCycle(graph))
 
