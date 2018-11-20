6496b4aa449c808c91c2456afc882b30dde98675
VeronikaBogdanovich <veronicka.angel2012@yandex.by>
2018-11-20 17:22:31 +0300

    finish crossroad task

--- CrossRoads/CrossRoad.py
+++ CrossRoads/CrossRoad.py
@@ -1,34 +1,43 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[93]:
+# In[254]:
 
 
 from math import inf
 
 def floydWarshall(graph): 
+    e=[0 for _ in range(N)]
+    rad=inf
     for k in range(N): 
         for i in range(N): 
             for j in range(N): 
                 graph[i][j] = min(graph[i][j] , graph[i][k]+ graph[k][j] ) 
-    print ("Following matrix shows the shortest distances\ between every pair of vertices" )
+    print ("Кратчайшие дороги между каждой парой дорог" )
     for i in graph:
         print(*i)
-#     for i in range(N): 
-#         for j in range(N): 
-#             e[i] = max(e[i], graph[i][j])
-#             print(e[i])
+    for i in range(N): 
+        for j in range(N): 
+            e[i]= max(e[i], graph[i][j])
+        print("Самый дальный перекресток для", i, "дороги:", j, ", время до него:", e[i])
+    for i in range(N):
+        rad = min(rad, e[i])
+        if (e[i] == rad):
+            c = i
+    print("пожарная станция может быть на перекрестке:",c)
 
 
-# In[94]:
+# In[255]:
 
 
 def addVertex(graph):
     N=len(graph)
     for i in range(N):
-        graph[i].append(inf)  
+        graph[i].append(inf)
     N += 1
     graph.append([inf]* N)
+    for i in range (N):
+        graph[i][i]=0
     for i in graph:
         print(*i)
         
@@ -36,12 +45,12 @@ def addEdge(graph):
     print('Введите вершины ребра: ')
     v1=(int(input())-1)
     v2=(int(input())-1)
-    print ('Введите вес ребра:')
+    print ('Введите вес:')
     weight=(int(input()))
     graph[v1][v2]=weight;
 
 
-# In[95]:
+# In[256]:
 
 
 # Задание графов(матрица инцидентности)
